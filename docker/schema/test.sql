894bc8bc68b4a036988a640e63f96580bafa102b
SovietBeast <pawlik1998@gmail.com>
2022-10-11 23:46:55 +0200

    Add docker-compose to start and init database

---
+++ docker/schema/test.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE chatterio;
+USE chatterio;
+
+CREATE TABLE users (
+    user_id int NOT NULL AUTO_INCREMENT,
+    username varchar(255) NOT NULL,
+    userpassword varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    PRIMARY KEY (user_id)
+);
+
+CREATE TABLE messages (
+    message_id int NOT NULL AUTO_INCREMENT,
+    message_from_user_id int NOT NULL,
+    message_to_user_id int NOT NULL,
+    message_text TEXT NOT NULL,
+    PRIMARY KEY (message_id)
+);
