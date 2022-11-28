af9b5c88064a631961bba61dba94ab9923c35c22
Nothron <arek.90002@gmail.com>
2022-11-28 20:11:50 +0100

    Added logout

---
+++ src/components/MainNavbar.vue
@@ -0,0 +1,41 @@
+<template>
+  <nav>
+    <span @click="logout">LOGOUT</span>
+  </nav>
+</template>
+
+<script>
+import { defineComponent } from "@vue/runtime-core"
+import { useRouter } from "vue-router"
+import { useUserStore } from "../store/userStore"
+
+export default defineComponent({
+  setup(){
+    const userStore = useUserStore();
+    const router = useRouter();
+    const logout = () => {
+      //authLogout();
+
+      userStore.setToken=null;
+      userStore.setUser=null;
+      localStorage.removeItem('token');
+      router.push('/login')
+    }
+    
+
+    return{
+      logout
+    }
+  },
+})
+</script>
+
+<style>
+nav{
+  position:sticky;
+  top:0;
+}
+.logout{
+  width:auto;
+}
+</style>
